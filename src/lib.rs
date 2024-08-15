6d902e8a6060007f4ab94cd78882247ae2558d96
Godot-Rust Automation <GodotRust@users.noreply.github.com>
2024-08-15 22:01:06 +0000

    Package gdextension-api v0.2.0

--- src/lib.rs
+++ src/lib.rs
@@ -4,16 +4,6 @@
 //! We do not offer any guarantees for the provided API.
 
 #![doc(html_logo_url = "https://godotengine.org/assets/press/icon_color.svg")]
-#[path = "../versions/4.0/src/lib.rs"]
-pub mod version_4_0;
-#[path = "../versions/4.0.1/src/lib.rs"]
-pub mod version_4_0_1;
-#[path = "../versions/4.0.2/src/lib.rs"]
-pub mod version_4_0_2;
-#[path = "../versions/4.0.3/src/lib.rs"]
-pub mod version_4_0_3;
-#[path = "../versions/4.0.4/src/lib.rs"]
-pub mod version_4_0_4;
 #[path = "../versions/4.1/src/lib.rs"]
 pub mod version_4_1;
 #[path = "../versions/4.1.1/src/lib.rs"]
@@ -30,3 +20,5 @@ pub mod version_4_2;
 pub mod version_4_2_1;
 #[path = "../versions/4.2.2/src/lib.rs"]
 pub mod version_4_2_2;
+#[path = "../versions/4.3/src/lib.rs"]
+pub mod version_4_3;
